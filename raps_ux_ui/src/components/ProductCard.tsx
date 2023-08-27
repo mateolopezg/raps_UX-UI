a1cff7a2a1913d6c42112a39625f5835a65c8d77
mate_lg@tutanota.com <M\r[mjKQU5@6V7N.F=4*/+(p?>
2023-08-26 23:37:30 -0400

    add more translate

--- raps_ux_ui/src/components/ProductCard.tsx
+++ raps_ux_ui/src/components/ProductCard.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { images } from "../utils/images";
+import { useTranslation } from "react-i18next";
+
 
 const ProductCard: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center my-10">
       {images.map((image) => (
@@ -17,10 +21,10 @@ const ProductCard: React.FC = () => {
             />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Macetas y plantas!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className="card-title">{t("products.title.card")}</h2>
+            <p>{t("products.title.description")}</p>
             <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+              <button className="btn btn-primary">{t("products.title.button")}</button>
             </div>
           </div>
         </div>

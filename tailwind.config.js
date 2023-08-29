24654c3a8f1c6e8f09f48d602c9b9512cddc3c81
mate_lg@tutanota.com <M\r[mjKQU5@6V7N.F=4*/+(p?>
2023-08-28 20:16:24 -0400

    repare deploy2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", './src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: 'class',
-  theme: {
-    extend: {
-      colors: {
-        'ultramar': '#0A2149'
-      }
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-}
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  plugins: [require('daisyui')],
+};
 
